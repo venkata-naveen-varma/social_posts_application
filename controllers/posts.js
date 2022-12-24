8822e750adc85cdb170361de4012d84697dc4df8
“venkata-naveen-varma” <venkata2naveen@gmail.com>
2022-12-24 17:12:26 -0500

    connection with mongoDB, mongoose schema creation

---
+++ controllers/posts.js
@@ -0,0 +1,22 @@
+import postMessage from '../models/postMessages.js';
+
+export const getPosts = async (req, res) => {
+    try{
+        const postMessages = await postMessage.find()
+        res.send(200).json(postMessages)
+    }catch(e){
+        res.send(404).json({message: e.message})
+    }
+}
+
+export const createPosts = async (req, res) => {
+    const post = req.body;
+    const newPost = new postMessage(post);
+    try{
+        await newPost.save();
+        res.status(201).json(newPost)
+    }catch(e){
+        res.status(409).json({message: e.message})
+    }
+    res.send('Post created')
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoute from './routes/posts.js'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use('/posts', postRoute)
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+mongoose.connect(process.env.DB_CONNECTION_URL)
+    .then(() => app.listen(port,(req,res)=>{console.log(`Server running on port ${port}`)}))
+    .catch((error) => console.log("Found the error --> ", error))
