b9236fb4b64002fb9856bf8fe4fb929b14aa2389
Venkata Naveen Varma Vegesna <115595566+venkata-naveen-varma@users.noreply.github.com>
2023-04-25 10:47:12 -0400

    Update postMessages.js

--- models/postMessages.js
+++ models/postMessages.js
@@ -7,7 +7,7 @@ const postSchema = mongoose.Schema({
     tags: [String],
     selectedFile: String,
     likeCount: {
-        type: Integer,
+        type: Number,
         default: 0
     },
     createdAt: {
